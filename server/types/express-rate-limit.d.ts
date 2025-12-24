68d3247e6a3cd461ed1757cf9ccc1a9b59323438
bzinkan <zinkan.brian@gmail.com>
2025-12-23 21:37:34 -0500

    .ts update

--- server/types/express-rate-limit.d.ts
+++ server/types/express-rate-limit.d.ts
@@ -1,9 +1,13 @@
-import "express";
+import "express-serve-static-core";
 
 declare module "express-serve-static-core" {
   interface Request {
     rateLimit?: {
+      limit?: number;
+      remaining?: number;
       resetTime?: Date;
     };
   }
 }
+
+export {};
